28733e4c7d304eb505a2a9cbf2a4421dd4bb75ef
yeyu <1010088794@qq.com>
2022-06-14 21:36:58 +0800

    Nacos集成

--- Order-server/src/main/java/com/demo/OrderApplication.java
+++ Order-server/src/main/java/com/demo/OrderApplication.java
@@ -2,8 +2,10 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = "com.demo")
+@EnableDiscoveryClient
 public class OrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class,args);
