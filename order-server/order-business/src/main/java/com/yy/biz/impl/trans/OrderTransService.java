99f2f3ac18a05aa313b1288da40549c4353d1f37
yeyu <1010088794@qq.com>
2022-06-22 16:00:32 +0800

    RocketMQ

--- order-server/order-business/src/main/java/com/yy/biz/impl/trans/OrderTransService.java
+++ order-server/order-business/src/main/java/com/yy/biz/impl/trans/OrderTransService.java
@@ -3,13 +3,13 @@ package com.yy.biz.impl.trans;
 import com.yy.bo.OrderCreateBO;
 import com.yy.dao.service.IOrderDetailService;
 import com.yy.dao.service.IOrderInfoService;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * @author yeyu
@@ -27,19 +27,24 @@ public class OrderTransService {
     @Resource
     private RestTemplate restTemplate;
 
+    @Resource
+    private RocketMQTemplate rocketMQTemplate;
+
     @Transactional
     public Boolean createOrder(OrderCreateBO bo) {
         boolean saveResult = orderInfoService.save(bo.getOrderInfo());
         boolean saveBatchResult = orderDetailService.saveBatch(bo.getOrderDetails());
-        //扣减库存
-        AtomicBoolean stockReduceResult = new AtomicBoolean(Boolean.TRUE);
-        bo.getStockReduceList().forEach(it -> {
-            Boolean success = restTemplate.postForObject("http://localhost:8000/stock/reduce", it, Boolean.class);
-            if (ObjectUtils.isEmpty(success) || !success) {
-                stockReduceResult.set(Boolean.FALSE);
-            }
-        });
-        boolean success = saveBatchResult && saveResult && stockReduceResult.get();
+        //扣减库存 发送mq消息
+        rocketMQTemplate.sendMessageInTransaction("REDUCE_STOCK",
+                MessageBuilder.withPayload(bo.getStockReduceList()).build(),bo.getStockReduceList());
+//        AtomicBoolean stockReduceResult = new AtomicBoolean(Boolean.TRUE);
+//        bo.getStockReduceList().forEach(it -> {
+//            Boolean success = restTemplate.postForObject("http://localhost:8000/stock/reduce", it, Boolean.class);
+//            if (ObjectUtils.isEmpty(success) || !success) {
+//                stockReduceResult.set(Boolean.FALSE);
+//            }
+//        });
+        boolean success = saveBatchResult && saveResult;
         // 模拟 调用服务成功 本地操作失败
         success = false;
         if (!success) {
